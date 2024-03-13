98db5a027876bf65fa4717ce563bd3ef05a7d32d
Sarthak <221b336@juetguna.in>
2024-03-13 14:20:03 +0530

    added priority with preemptive

---
+++ scheduling-algorithms/lottery-scheduling.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+
+	int n;
+	printf("Enter size: ");
+	scanf("%d" , &n);
+
+	int p[5] = {1 , 2 , 3 , 4 , 5};
+	int tickets[5] = {60 , 20 , 80 , 30 , 10};
+	
+	
+
+	return 0;
+}
